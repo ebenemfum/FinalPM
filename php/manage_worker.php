0dd6c916fd5dce5c25712accf22fea039fea7e46
ebenemfum <ebenezer.amoah@ashesi.edu.gh>
2021-12-09 23:56:44 -0800

    Uploaded

--- php/manage_worker.php
+++ php/manage_worker.php
@@ -4,7 +4,7 @@
   if($con) {
     if(isset($_GET["action"]) && $_GET["action"] == "delete") {
       $id = $_GET["id"];
-      $query = "DELETE FROM workers WHERE ID = $id";
+      $query = "DELETE FROM Workers WHERE ID = $id";
       $result = mysqli_query($con, $query);
       if(!empty($result))
     		showWorkers(0);
@@ -21,33 +21,33 @@
       $email = $_GET["email"];
       $contact_number = $_GET["contact_number"];
       $address = ucwords($_GET["address"]);
-      updateworker($id, $name, $email, $contact_number, $address);
+      updateWorker($id, $name, $email, $contact_number, $address);
     }
 
     if(isset($_GET["action"]) && $_GET["action"] == "cancel")
       showWorkers(0);
 
     if(isset($_GET["action"]) && $_GET["action"] == "search")
-      searchworker(strtoupper($_GET["text"]));
+      searchWorker(strtoupper($_GET["text"]));
   }
 
   function showWorkers($id) {
     require "db_connection.php";
     if($con) {
       $seq_no = 0;
-      $query = "SELECT * FROM workers";
+      $query = "SELECT * FROM Workers";
       $result = mysqli_query($con, $query);
       while($row = mysqli_fetch_array($result)) {
         $seq_no++;
         if($row['ID'] == $id)
           showEditOptionsRow($seq_no, $row);
         else
-          showworkerRow($seq_no, $row);
+          showWorkerRow($seq_no, $row);
       }
     }
   }
 
-  function showworkerRow($seq_no, $row) {
+  function showWorkerRow($seq_no, $row) {
     ?>
     <tr>
       <td><?php echo $seq_no; ?></td>
@@ -57,10 +57,10 @@
       <td><?php echo $row['CONTACT_NUMBER']; ?></td>
       <td><?php echo $row['ADDRESS']; ?></td>
       <td>
-        <button href="" class="btn btn-info btn-sm" onclick="editworker(<?php echo $row['ID']; ?>);">
+        <button href="" class="btn btn-info btn-sm" onclick="editWorker(<?php echo $row['ID']; ?>);">
           <i class="fa fa-pencil"></i>
         </button>
-        <button class="btn btn-danger btn-sm" onclick="deleteworker(<?php echo $row['ID']; ?>);">
+        <button class="btn btn-danger btn-sm" onclick="deleteWorker(<?php echo $row['ID']; ?>);">
           <i class="fa fa-trash"></i>
         </button>
       </td>
@@ -74,22 +74,22 @@ function showEditOptionsRow($seq_no, $row) {
     <td><?php echo $seq_no; ?></td>
     <td><?php echo $row['ID'] ?></td>
     <td>
-      <input type="text" class="form-control" value="<?php echo $row['NAME']; ?>" placeholder="Name" id="worker_name" onkeyup="validateName(this.value, 'name_error');">
+      <input type="text" class="form-control" value="<?php echo $row['NAME']; ?>" placeholder="Name" id="Worker_name" onkeyup="validateName(this.value, 'name_error');">
       <code class="text-danger small font-weight-bold float-right" id="name_error" style="display: none;"></code>
     </td>
     <td>
-      <input type="email" class="form-control" value="<?php echo $row['EMAIL']; ?>" placeholder="Email" id="worker_email" onblur="validateContactNumber(this.value, 'email_error');">
+      <input type="email" class="form-control" value="<?php echo $row['EMAIL']; ?>" placeholder="Email" id="Worker_email" onblur="validateContactNumber(this.value, 'email_error');">
     </td>
     <td>
-      <input type="number" class="form-control" value="<?php echo $row['CONTACT_NUMBER']; ?>" placeholder="Contact Number" id="worker_contact_number" onblur="validateContactNumber(this.value, 'contact_number_error');">
+      <input type="number" class="form-control" value="<?php echo $row['CONTACT_NUMBER']; ?>" placeholder="Contact Number" id="Worker_contact_number" onblur="validateContactNumber(this.value, 'contact_number_error');">
       <code class="text-danger small font-weight-bold float-right" id="contact_number_error" style="display: none;"></code>
     </td>
     <td>
-      <textarea class="form-control" placeholder="Address" id="worker_address" onblur="validateAddress(this.value, 'address_error');"><?php echo $row['ADDRESS']; ?></textarea>
+      <textarea class="form-control" placeholder="Address" id="Worker_address" onblur="validateAddress(this.value, 'address_error');"><?php echo $row['ADDRESS']; ?></textarea>
       <code class="text-danger small font-weight-bold float-right" id="address_error" style="display: none;"></code>
     </td>
     <td>
-      <button href="" class="btn btn-success btn-sm" onclick="updateworker(<?php echo $row['ID']; ?>);">
+      <button href="" class="btn btn-success btn-sm" onclick="updateWorker(<?php echo $row['ID']; ?>);">
         <i class="fa fa-edit"></i>
       </button>
       <button class="btn btn-danger btn-sm" onclick="cancel();">
@@ -100,23 +100,23 @@ function showEditOptionsRow($seq_no, $row) {
   <?php
 }
 
-function updateworker($id, $name, $email, $contact_number, $address) {
+function updateWorker($id, $name, $email, $contact_number, $address) {
   require "db_connection.php";
-  $query = "UPDATE workers SET NAME = '$name', EMAIL = '$email', CONTACT_NUMBER = '$contact_number', ADDRESS = '$address' WHERE ID = $id";
+  $query = "UPDATE Workers SET NAME = '$name', EMAIL = '$email', CONTACT_NUMBER = '$contact_number', ADDRESS = '$address' WHERE ID = $id";
   $result = mysqli_query($con, $query);
   if(!empty($result))
     showWorkers(0);
 }
 
-function searchworker($text) {
+function searchWorker($text) {
   require "db_connection.php";
   if($con) {
     $seq_no = 0;
-    $query = "SELECT * FROM workers WHERE UPPER(NAME) LIKE '%$text%'";
+    $query = "SELECT * FROM Workers WHERE UPPER(NAME) LIKE '%$text%'";
     $result = mysqli_query($con, $query);
     while($row = mysqli_fetch_array($result)) {
       $seq_no++;
-      showworkerRow($seq_no, $row);
+      showWorkerRow($seq_no, $row);
     }
   }
 }
